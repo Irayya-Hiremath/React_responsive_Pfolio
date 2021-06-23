b88a3adb8f73a53adf2f770a4e8cf849c946fdec
Irayya-Hiremath <ihiremath1991@gmail.com>
2021-06-24 00:24:14 +0530

    pages added

--- src/Components/NavMenu.js
+++ src/Components/NavMenu.js
@@ -1,5 +1,26 @@
 import React from "react"
 import {NavLink} from "react-router-dom"
+import styled from "styled-components";
+
+
+const NavMenuStyles=styled.div `
+postion:fixed;
+z-index:100;
+top:0;
+width:100%;
+padding:1rem 0;
+ul{
+    max-width:1200px;
+    margin:0 auto;
+    text-align:center;
+    li{
+        diplay:inline-block;
+        border-raduis:8px;
+        transition:.3s ease background-color;
+    }
+}
+`;
+
 
 
 function NavMenu(){
